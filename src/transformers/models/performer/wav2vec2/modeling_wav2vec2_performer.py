48a7609526d0dc0f8d1fb7fae62b8b37e3b665da
Lorenzo Valgimigli <lorenzo.valgimigli@studio.unibo.it>
2021-06-01 09:25:53 +0200

    test 1: ho cambiato la shape delle Q,K,V matrici. in particolare ho modificato proj_shape

--- src/transformers/models/performer/wav2vec2/modeling_wav2vec2_performer.py
+++ src/transformers/models/performer/wav2vec2/modeling_wav2vec2_performer.py
@@ -293,7 +293,7 @@ class Wav2Vec2Attention(nn.Module):
             # if encoder bi-directional self-attention `past_key_value` is always `None`
             past_key_value = (key_states, value_states)
 
-        proj_shape = (bsz * self.num_heads, -1, self.head_dim)
+        proj_shape = (bsz, self.num_heads, -1, self.head_dim)
         query_states = self._shape(query_states, tgt_len, bsz).view(*proj_shape)
         key_states = key_states.view(*proj_shape)
         value_states = value_states.view(*proj_shape)
