ec6efd5989df72641808d7763e4663615adfb157
Lorenzo Valgimigli <lorenzo.valgimigli@studio.unibo.it>
2021-06-04 17:57:20 +0200

    commented quadratic attention code, fixed attention mask shape with bsz, 1, seq_len, 1. Fixed attention_weights_reshaped

--- src/transformers/models/performer/wav2vec2/modeling_wav2vec2_performer.py
+++ src/transformers/models/performer/wav2vec2/modeling_wav2vec2_performer.py
@@ -330,7 +330,7 @@ class Wav2Vec2Attention(nn.Module):
         #    attn_weights = attn_weights_reshaped.view(bsz * self.num_heads, tgt_len, src_len)
         #else:
         #    attn_weights_reshaped = None
-
+        attn_weights_reshaped = None
         # attn_probs = F.dropout(attn_weights, p=self.dropout, training=self.training)
         performer_shape_proj = (bsz, self.num_heads, -1, self.head_dim)
         attention_mask_shape_proj = (bsz, 1, -1, 1)   #bsz, 1, seq_len, 1
